abc27a10805207a4e3a5a1a6299cc7f3741b99ae
Dang <haidangk63@gmail.com>
2020-12-20 15:38:23 +0700

    update query

--- src/services/DongGopService.java
+++ src/services/DongGopService.java
@@ -4,7 +4,7 @@ package services;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
@@ -13,27 +13,21 @@ import com.mysql.jdbc.Statement;
 import models.DanhSachDongGop;
 import models.DanhSachKhoanThu;
 import models.HoDongGop;
+import models.HoNopTien;
 
 public class DongGopService {
-	public boolean addNew(DanhSachDongGop dsDongGop) throws ClassNotFoundException {
+	
+	public boolean themDongGop(DanhSachDongGop dsDongGop) {
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
-			String sqlInsert = "INSERT INTO danhsachdonggop( tenDongGop, batDau, ketThuc, TongTien)" 
-	                + " values (?, ?, ?, ?)";
-			String sqlCreateTable = "CREATE TABLE " + dsDongGop.getTenDongGop().replaceAll(" ", "_") + " " +
-					"(idHoKhau numeric not NULL, " +
-					"tenHoKhau varchar(255), " +
-					"tienDongGop varchar(255), " +
-					"PRIMARY KEY (idHoKhau))";
+			String sqlInsert = "INSERT INTO dong_gop(ten_dong_gop, ngay_bat_dau, ngay_ket_thuc)" 
+	                + " values (?, ?, ?)";
+			
 			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlInsert);
 			preparedStatement.setString(1, dsDongGop.getTenDongGop());
 			preparedStatement.setString(2, dsDongGop.getBatDau());
 			preparedStatement.setString(3, dsDongGop.getKetThuc());
-			preparedStatement.setInt(4, dsDongGop.getTongTien());
 			preparedStatement.execute();
-			
-			preparedStatement = (PreparedStatement) connection.prepareStatement(sqlCreateTable);
-			preparedStatement.executeUpdate();
 			
 			preparedStatement.close();
 	        connection.close();
@@ -46,21 +40,25 @@ public class DongGopService {
 		return true;
 	}
 	
-	public void themHoKhau(DanhSachDongGop dsDongGop, List<HoDongGop> hoDongGop) {
+	public void themHoKhau(DanhSachDongGop dsDongGop, ArrayList<HoDongGop> hoDongGop) {
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
+			String sqlSearch = "select ID from khoan_thu where ten_dong_gop = " + "'" + dsDongGop.getTenDongGop() + "'";
+			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlSearch);
+			ResultSet rs = preparedStatement.executeQuery();
+			int idDongGop = rs.getInt("ID");
+			String sqlInsert = "INSERT INTO quan_ly_dong_gop(id_donggop, id_hokhau, so_tien_dong)"
+					+ " values (?, ?, ?)";
+			preparedStatement = (PreparedStatement) connection.prepareStatement(sqlInsert);
 			for(int i = 0; i < hoDongGop.size(); i++) {
-				String sqlInsert = "INSERT INTO"+ dsDongGop.getTenDongGop().replaceAll(" ", "_")
-						+ " values (?, ?)";
-				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlInsert);
-				preparedStatement.setInt(1, hoDongGop.get(i).getIdHoKhau());
-				preparedStatement.setString(2, hoDongGop.get(i).getTenHoKhau());
+				preparedStatement.setInt(1, idDongGop);
+				preparedStatement.setInt(2, hoDongGop.get(i).getIdHoKhau());
+				preparedStatement.setInt(3, hoDongGop.get(i).getTienDongGop());
 				
 				preparedStatement.execute();
-				
-				preparedStatement.close();
-				connection.close();
 			}
+			preparedStatement.close();
+			connection.close();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,23 +68,50 @@ public class DongGopService {
 		}
 	}
 	
-	public List<HoDongGop> getHoDongGop() {
-		List<HoDongGop> hoDongGop = null;
+	public ArrayList<DanhSachDongGop> getDanhSach() {
+		ArrayList<DanhSachDongGop> dsDongGop = new ArrayList<DanhSachDongGop>();
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
-			String sqlSelect = "select  idHoKhau, tenHoKhau from hokhau";
+			String sqlSelect = "select * from dong_gop";
+			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlSelect);
+			ResultSet rs = preparedStatement.executeQuery();
+			while(rs.next()) {
+				DanhSachDongGop dsDG = new DanhSachDongGop();
+				dsDG.setIdDongGop(rs.getInt("ID"));
+				dsDG.setTenDongGop(rs.getString("ten_dong_gop"));
+				dsDG.setBatDau(rs.getString("ngay_bat_dau"));
+				dsDG.setKetThuc(rs.getString("ngay_ket_thuc"));
+				dsDongGop.add(dsDG);
+			}
+			
+			preparedStatement.close();
+			connection.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return dsDongGop;
+	}
+	
+	public ArrayList<HoDongGop> getHoDongGop() {
+		ArrayList<HoDongGop> hoDongGop = new ArrayList<HoDongGop>();
+		try {
+			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
+			String sqlSelect = "select ID, ten_chu_ho from ho_khau";
 			PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(sqlSelect);
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
 				HoDongGop hoDG = new HoDongGop();
-				hoDG.setIdHoKhau(rs.getInt("idHoKhau"));
-				hoDG.setTenHoKhau(rs.getString("tenHoKhau"));
+				hoDG.setIdHoKhau(rs.getInt("ID"));
+				hoDG.setTenHoKhau(rs.getString("ten_chu_ho"));
+				hoDG.setTienDongGop(0);
 				hoDongGop.add(hoDG);
 			}
 			
 			stmt.close();
 			connection.close();
-			
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -97,20 +122,18 @@ public class DongGopService {
 		return hoDongGop;
 	}
 	
-	public void capNhatDanhSach(DanhSachDongGop dsDongGop) {
+	public void capNhatDongGop(DanhSachDongGop dsDongGop) {
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
-			String sqlUpdate = "update danhsachdonggop set "
-					+ "tenDongGop = ?, "
-					+ "batDau = ?, "
-					+ "ketThuc = ?, " 
-					+ "tongTien = ? "
-					+ "where idDongGop = " + dsDongGop.getIdDongGop();
+			String sqlUpdate = "update dong_gop set "
+					+ "ten_dong_gop = ?, "
+					+ "ngay_bat_dau = ?, "
+					+ "ngay_ket_thuc = ?, " 
+					+ "where ID = " + dsDongGop.getIdDongGop();
 			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlUpdate);
 			preparedStatement.setString(1, dsDongGop.getTenDongGop());
 			preparedStatement.setString(2, dsDongGop.getBatDau());
 			preparedStatement.setString(3, dsDongGop.getKetThuc());
-			preparedStatement.setInt(4, dsDongGop.getTongTien());
 			preparedStatement.execute();
 			
 			preparedStatement.close();
@@ -123,21 +146,21 @@ public class DongGopService {
 		}
 	}
 	
-	public void capNhatHoDongHop(DanhSachDongGop dsDongGop, HoDongGop hoDongGop) {
+	public void capNhatHoDongGop(DanhSachDongGop dsDongGop, ArrayList<HoDongGop> hoDongGop) {
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
-			String sqlUpdate = "update " + dsDongGop.getTenDongGop().replaceAll(" ", "_") + " set "
-					+ "idHoKhau = ?, "
-					+ "tenHoKhau = ?, "
-					+ "tienDongGop = ?, "
-					+ "where idHoKhau = " + hoDongGop.getIdHoKhau();
-			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlUpdate);
-			preparedStatement.setInt(1, hoDongGop.getIdHoKhau());
-			preparedStatement.setString(2, hoDongGop.getTenHoKhau());
-			preparedStatement.setInt(3, hoDongGop.getTienDongGop());
-			preparedStatement.execute();
+			for(int i = 0; i < hoDongGop.size(); i++) {
+				String sqlUpdate = "update quan_ly_dong_gop" + " set "
+						+ "so_tien_dong = ?, " + hoDongGop.get(i).getTienDongGop()
+						+ "where id_hokhau = " + hoDongGop.get(i).getIdHoKhau()
+						+ "and id_donggop = " + dsDongGop.getIdDongGop();
+				PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlUpdate);
+//				preparedStatement.setInt(1, hoNopTien.get(i).getSoNhanKhau());
+				preparedStatement.execute();
+				
+				preparedStatement.close();
+			}
 			
-			preparedStatement.close();
 			connection.close();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -146,21 +169,16 @@ public class DongGopService {
 		}
 	}
 	
-	public DanhSachDongGop searchDSKhoanThu(String tenDongGop) {
-		DanhSachDongGop dsDongGop = new DanhSachDongGop();
+	public int tongTienThu(DanhSachDongGop dsDongGop) {
+		int sum = 0;
 		try {
 			Connection connection = (Connection) MysqlConnection.getMysqlConnection();
-			String sqlSelect = "select  idDongGop, tenDongGop, batDau, ketThuc, tongTien" 
-			+ " from danhsachdonggop"
-			+ " where tenDongGop = " + "'" + tenDongGop + "'";
+			String sqlSelect = "select SUM(so_tien_dong) tong_tien from quan_ly_dong_gop "
+					+ "where id_donggop = " + dsDongGop.getIdDongGop();
 			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sqlSelect);
 			ResultSet rs = preparedStatement.executeQuery();
-			
-			dsDongGop.setIdDongGop(rs.getInt("idDongGop"));
-			dsDongGop.setTenDongGop(rs.getString("tenDongGop"));
-			dsDongGop.setBatDau(rs.getString("batDau"));
-			dsDongGop.setKetThuc(rs.getString("ketThuc"));
-			dsDongGop.setTongTien(rs.getInt("tongTien"));
+			rs.first();
+			sum = rs.getInt("tong_tien");
 			
 			preparedStatement.close();
 			connection.close();
@@ -169,6 +187,6 @@ public class DongGopService {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return dsDongGop;
+		return sum;
 	}
 }
